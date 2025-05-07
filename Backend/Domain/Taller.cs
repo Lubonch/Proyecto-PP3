2473d8c532ef4868d9f93496a50ef427f8ac2ec0
lubonch <lucasbonch@gmail.com>
2025-05-07 20:30:51 -0300

    Se modifica el objeto Taller para que represente la entidad Taller en la base de datos.
    Se actualiza el backup de la base de datos

--- Backend/Domain/Taller.cs
+++ Backend/Domain/Taller.cs
@@ -2,8 +2,14 @@
 {
     public class Taller
     {
-        public virtual int id { get; set; }
-        public virtual required string nombre { get; set; }
-        public virtual required string descripcion { get; set; }
+        public virtual int Id { get; set; }
+        public virtual required string Nombre { get; set; }
+        public virtual required string Descripcion { get; set; }
+        public virtual required DateTime FechaAlta { get; set; }
+        public virtual required string UsuarioAlta { get; set; }
+        public virtual DateTime FechaModif { get; set; }
+        public virtual string? UsuarioModif { get; set; }
+        public virtual Boolean Activo {  get; set; }
+
     }
 }
