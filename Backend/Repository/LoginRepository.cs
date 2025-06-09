e3be1cc5bf8d600d2d2ca6d868ed007c912ace83
lubonch <lucasbonch@gmail.com>
2025-06-08 22:45:17 -0300

    Cambios para el registro de usuarios

--- Backend/Repository/LoginRepository.cs
+++ Backend/Repository/LoginRepository.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System;
 using Microsoft.Data.SqlClient;
 using System.Data;
+using Azure.Core;
 
 namespace Backend.Repository
 {
@@ -40,6 +41,42 @@ namespace Backend.Repository
             }
 
             return products;
+        }
+
+        public Boolean Registro(Registro dataRegistro)
+        {
+
+            IConfigurationRoot _configuration = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json")
+           .Build();
+
+            var FechaRegistro = DateTime.Now;
+            var activo = true;
+            var administrador = false;
+
+            string connString = _configuration.GetConnectionString("BackendDatabase")!;
+            //var connString = app.Configuration.GetConnectionString("MangaCountDatabase");
+
+            var parameters = new DynamicParameters();
+            parameters.Add("@Usuario", dataRegistro.Usuario, DbType.AnsiString, ParameterDirection.Input, dataRegistro.Usuario.Length);
+            parameters.Add("@Password", dataRegistro.Password, DbType.AnsiString, ParameterDirection.Input, dataRegistro.Password.Length);
+            parameters.Add("@Email", dataRegistro.Email, DbType.AnsiString, ParameterDirection.Input, dataRegistro.Email.Length);
+            parameters.Add("@TallerId", null, DbType.AnsiString, ParameterDirection.Input);
+            parameters.Add("@FechaRegistro", FechaRegistro, DbType.AnsiString, ParameterDirection.Input);
+            parameters.Add("@Activo", activo, DbType.AnsiString, ParameterDirection.Input, dataRegistro.Email.Length);
+            parameters.Add("@Administrador", administrador, DbType.AnsiString, ParameterDirection.Input);
+
+
+            var sql = "insert into [dbo].[Alumno]([Usuario],[Password],[Email],[TallerId],[FechaRegistro],[Activo],[Administrador]) VALUES(@Usuario,@Password,@Email,@TallerId,@FechaRegistro,@Activo,@Administrador) SELECT SCOPE_IDENTITY();";
+            var result = new List<User>();
+            using (var connection = new SqlConnection(connString))
+            {
+                connection.Open();
+                result = connection.Query<User>(sql, parameters).ToList();
+            }
+
+            return result.Any();
         }
     }
 }
